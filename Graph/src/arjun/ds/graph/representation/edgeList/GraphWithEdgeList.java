8d3a136390cfb7b8e0257df69b279b092f051ab9
giddamr <giddamr@GIDDAMR2C5S542.opentext.net>
2019-07-28 10:41:27 +0530

    - EdgeList representation - Added findAllAdjacentEdges & findIsThereEdge methods

--- Graph/src/arjun/ds/graph/representation/edgeList/GraphWithEdgeList.java
+++ Graph/src/arjun/ds/graph/representation/edgeList/GraphWithEdgeList.java
@@ -2,6 +2,7 @@ package arjun.ds.graph.representation.edgeList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class Edge {
 	int startVertex;
@@ -12,6 +13,11 @@ class Edge {
 		this.startVertex = startVertex;
 		this.endVertex = endVertex;
 		this.weight = weight;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + startVertex + "," + endVertex + ")";
 	}
 
 }
@@ -30,14 +36,35 @@ public class GraphWithEdgeList {
 	}
 
 	void printVertices() {
+		System.out.println("All vertices in the Graph: ");
 		vertext_list.forEach(i -> System.out.print(i + " "));
 	}
 
 	void printEdges() {
-		System.out.print("\n[");
-		edge_list.forEach(e -> System.out
-				.print("(" + vertext_list.get(e.startVertex) + ", " + vertext_list.get(e.endVertex) + ") "));
+		System.out.print("\n\nAll edges in the Graph: \n[");
+		edge_list.forEach(e -> System.out.print(
+				"(" + vertext_list.get(e.startVertex) + ", " + vertext_list.get(e.endVertex) + ", " + e.weight + ") "));
 		System.out.println("]");
+	}
+
+	boolean findIsThereEdge(String v1, String v2) {
+
+		int index1 = vertext_list.indexOf(v1);
+		int index2 = vertext_list.indexOf(v2);
+
+		return edge_list.stream().anyMatch((e) -> (e.startVertex == index1 && e.endVertex == index2
+				|| e.startVertex == index2 && e.endVertex == index1));
+
+	}
+
+	void findAllAdjacentEdges(String v) {
+		int index = vertext_list.indexOf(v);
+		System.out.print("[");
+		edge_list.stream().filter((e) -> (e.startVertex == index || e.endVertex == index)).collect(Collectors.toList())
+				.forEach(e -> System.out
+						.print("(" + vertext_list.get(e.startVertex) + ", " + vertext_list.get(e.endVertex) + ", " + e.weight + ")"));
+		System.out.println("]");
+
 	}
 
 	public static void main(String[] args) {
@@ -50,11 +77,33 @@ public class GraphWithEdgeList {
 		graph.addVertex("F");
 		graph.addVertex("G");
 		graph.addVertex("H");
+
 		graph.printVertices();
+
 		graph.addEdge("A", "B", 5);
 		graph.addEdge("A", "C", 7);
 		graph.addEdge("A", "D", 3);
+		graph.addEdge("B", "E", 2);
+		graph.addEdge("B", "F", 10);
+		graph.addEdge("C", "G", 1);
+		graph.addEdge("D", "H", 11);
+		graph.addEdge("E", "H", 9);
+		graph.addEdge("F", "H", 4);
+		graph.addEdge("G", "H", 6);
+
 		graph.printEdges();
+
+		System.out.println("\nFind Edge between A and D: " + graph.findIsThereEdge("A", "D"));
+		System.out.println("Find Edge between A and G: " + graph.findIsThereEdge("A", "G"));
+		System.out.println("Find Edge between G and H: " + graph.findIsThereEdge("G", "H"));
+		System.out.println("Find Edge between G and H: " + graph.findIsThereEdge("X", "H"));
+		
+		System.out.println("\nAll the adjacent edges to A: ");
+		graph.findAllAdjacentEdges("A");
+		
+		System.out.println("\nAll the adjacent edges to H: ");
+		graph.findAllAdjacentEdges("H");
+
 	}
 
 }
