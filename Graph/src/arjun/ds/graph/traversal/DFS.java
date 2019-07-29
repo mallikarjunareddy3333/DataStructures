02778270bfd5f2c4a6351810b6e2e5ab509dbc6a
giddamr <giddamr@GIDDAMR2C5S542.opentext.net>
2019-07-29 15:25:03 +0530

    - DFS for Graph traversal using stack

--- Graph/src/arjun/ds/graph/traversal/DFS.java
+++ Graph/src/arjun/ds/graph/traversal/DFS.java
@@ -2,6 +2,7 @@ package arjun.ds.graph.traversal;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Stack;
 
 class Graph1 {
 
@@ -34,10 +35,10 @@ class Graph1 {
 			System.out.println("\n");
 		}
 	}
-	
+
 	// A function used by DFS
 	void DFSUtil(int s, boolean visited[]) {
-		
+
 		// Mark the current node as visited and print it
 		visited[s] = true;
 		System.out.print(s + " ");
@@ -58,10 +59,38 @@ class Graph1 {
 		// Mark all the vertices as not visited(By default
 		// set as false)
 		boolean visited[] = new boolean[V];
-		
-		// Call the recursive helper function to print DFS traversal 
+
+		// Call the recursive helper function to print DFS traversal
 		DFSUtil(s, visited);
+
+	}
+
+
+	void DFS_Iterate(int s) {
+		boolean visited[] = new boolean[V];
 		
+		Stack<Integer> stack = new Stack<>();
+		stack.push(s);
+
+		while (!stack.empty()) {
+			s = stack.peek();
+			stack.pop();
+
+			if (visited[s] == false) {
+				visited[s] = true;
+				System.out.print(s + " ");
+			}
+
+			Iterator<Integer> it = adjacentList[s].iterator();
+			while (it.hasNext()) {
+				int n = it.next();
+				if (!visited[n]) {
+					stack.push(n);
+				}
+			}
+
+		}
+
 	}
 
 }
@@ -70,22 +99,26 @@ public class DFS {
 
 	public static void main(String[] args) {
 
-		Graph1 g = new Graph1(4);
-
-		g.addEdge(0, 1);
-		g.addEdge(0, 2);
-		g.addEdge(1, 2);
-		g.addEdge(2, 0);
-		g.addEdge(2, 3);
-		g.addEdge(3, 3);
+		// Total 5 vertices in graph
+		Graph1 g = new Graph1(5);
+		
+		g.addEdge(1, 0); 
+        g.addEdge(2, 1); 
+        g.addEdge(3, 4); 
+        g.addEdge(4, 0);
 
 		// print the adjacency list representation of
 		// the above graph
 		g.printGraph(g);
 
 		System.out.println("Following is Breadth First Traversal " + "(starting from vertex 2)");
+		
+		System.out.print("Recursive: ");
+		g.DFS(0);
 
-		g.DFS(2);
+		System.out.println();
+		System.out.print("Iterative: ");
+		g.DFS_Iterate(0);
 
 	}
 
